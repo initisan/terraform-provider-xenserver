02f0946c01a745032c9fbf378427ea52713a8662
Jason chang <initisan@outlook.com>
2024-05-31 10:43:38 +0800

    tadf

--- .github/actions/setup-xenserver-sdk/extract_sdk_version.py
+++ .github/actions/setup-xenserver-sdk/extract_sdk_version.py
@@ -3,7 +3,13 @@ import re
 import os
 from bs4 import BeautifulSoup
 
-response = requests.get('https://www.xenserver.com/downloads')
+try:
+    response = requests.get('https://www.xenserver.com/downloads')
+    response.raise_for_status()
+except requests.exceptions.RequestException as err:
+    print ("OOps: Something Else Happened",err)
+    exit()
+
 soup = BeautifulSoup(response.text, 'html.parser')
 
 pattern = re.compile(r'Software Development Kit \(SDK\) (\d+\.\d+\.\d+)')
@@ -13,4 +19,5 @@ for text in soup.stripped_strings:
     if match:
         version = match.group(1)
         sdk_url = f'https://downloads.xenserver.com/sdk/{version}/XenServer-SDK-{version}.zip'
-        os.env.GITHUB_OUTPUT = f'XENSERVER_SDK_URL={sdk_url}'
+        os.environ['GITHUB_OUTPUT'] = f'XENSERVER_SDK_URL={sdk_url}'
+        break
